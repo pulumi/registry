3c5de055895a1eed1fd600c28700d952db612108
Christian Nunciato <c@nunciato.org>
2021-09-03 08:03:28 -0700

    Use the new Pulumi theme module (#3)
    Also adds unstyled filter components.

--- scripts/common.sh
+++ scripts/common.sh
@@ -2,10 +2,3 @@
 
 BUILD_IDENTIFIER="$(git rev-parse HEAD)"
 export REPO_THEME_PATH="themes/default/"
-export LOCAL_THEME_PATH="_vendor/github.com/pulumi/pulumi-hugo/themes/default/"
-
-# Bundle filenames are exported for use with Hugo and theme scripts.
-export CSS_BUNDLE="static/css/styles.${BUILD_IDENTIFIER}.css"
-export JS_BUNDLE="static/js/bundle.min.${BUILD_IDENTIFIER}.js"
-export REL_CSS_BUNDLE="/css/styles.${BUILD_IDENTIFIER}.css"
-export REL_JS_BUNDLE="/js/bundle.min.${BUILD_IDENTIFIER}.js"

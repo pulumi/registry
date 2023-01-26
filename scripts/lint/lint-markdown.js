1e39a6825a1a263e53fb29fdd106f6be91670276
Sean Holung <sean.holung@gmail.com>
2023-01-26 09:35:40 -0800

    test

--- scripts/lint/lint-markdown.js
+++ scripts/lint/lint-markdown.js
@@ -240,8 +240,6 @@ const opts = {
     config: {
         // Allow inline HTML.
         MD033: false,
-        // Do not enforce line length.
-        MD013: false,
         // Don't force language specification on code blocks.
         MD040: false,
         // Allow hard tabs.
@@ -262,8 +260,6 @@ const opts = {
         MD023: false,
         // Allow blank lines in blockquotes.
         MD028: false,
-        // Allow indentation in unordered lists.
-        MD007: false,
     },
     customRules: [
         {

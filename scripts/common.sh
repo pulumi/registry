c8686ef6609ba5473f2c0f2b88db4976cbc4ef18
Sean Holung <sean.holung@gmail.com>
2023-05-18 13:01:01 -0700

    test

--- scripts/common.sh
+++ scripts/common.sh
@@ -2,3 +2,11 @@
 
 BUILD_IDENTIFIER="$(git rev-parse HEAD)"
 export REPO_THEME_PATH="themes/default/"
+
+
+list_pull_requests() {
+    curl -L   -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+    -H "X-GitHub-Api-Version: 2022-11-28"   https://api.github.com/repos/pulumi/registry/pulls \
+    | jq -c '[.[] | .title]'
+}

40ad127a2caea0663168eb8cff0e15f7b087c622
Sean Holung <sean.holung@gmail.com>
2023-08-29 11:57:42 -0700

    add theme to registry

---
+++ themes/default/theme/src/ts/main.ts
@@ -0,0 +1,27 @@
+import { defineCustomElements } from "../../stencil/dist";
+
+import "../scss/main.scss";
+
+import "./misc";
+import "./nav";
+import "./carousel";
+import "./chooser";
+import "./price-toggle";
+import "./noselect";
+import "./tracking";
+import "./docs-feedback";
+import "./event-filtering";
+import "./copybutton";
+import "./code-tabbed";
+import "./resources";
+import "./packages";
+import "./pricing-trial";
+import "./developer-advocates";
+import "./toc";
+import "./ai";
+import "./docs-main";
+import "./redirects";
+import "./algolia/autocomplete";
+
+// Register all Stencil components.
+defineCustomElements();

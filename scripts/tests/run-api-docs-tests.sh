64a04bca92cffe27a1ac8add88869fb8360f1305
Sean Holung <sean.holung@gmail.com>
2024-07-19 12:11:28 -0700

    temp
    temp
    
    temp
    
    temp
    
    temp

--- scripts/tests/run-api-docs-tests.sh
+++ scripts/tests/run-api-docs-tests.sh
@@ -5,9 +5,10 @@
 
 # bucket="$(curl -s https://www.pulumi.com/registry/metadata.json | jq -r '.bucket' || echo '')"
 
-bucket="registry-production-origin-push-c9412e72"
+bucket="registry-production-origin-push-c530c8d7"
 
-aws s3 cp "s3://${bucket}/registry" public --recursive
+echo "Downloading current site to public dir...."
+aws s3 cp "s3://${bucket}/registry" public --recursive --quiet
 
-npm run api-docs-tests
+npm run test-api-docs
 

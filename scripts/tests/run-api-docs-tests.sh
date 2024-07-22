065abeac669bc5ece346db96a16d5a7a2e3c23e0
Sean Holung <sean.holung@gmail.com>
2024-07-22 12:03:54 -0700

    temp

--- scripts/tests/run-api-docs-tests.sh
+++ scripts/tests/run-api-docs-tests.sh
@@ -5,10 +5,19 @@
 
 # bucket="$(curl -s https://www.pulumi.com/registry/metadata.json | jq -r '.bucket' || echo '')"
 
-bucket="registry-production-origin-push-c530c8d7"
+bucket="registry-production-origin-push-c4c4803f"
 
 echo "Downloading current site to public dir...."
-aws s3 cp "s3://${bucket}/registry" public --recursive --quiet
+aws s3 cp "s3://${bucket}/registry" public/registry --recursive --quiet
+
+
+# pkgs=("aws" "gcp" "azure")
+
+# Iterate over the array
+# for str in "${pkgs[@]}"; do
+#     # Invoke the command with the string as the first argument
+#     npm run test-api-docs $str
+# done
 
 npm run test-api-docs
 

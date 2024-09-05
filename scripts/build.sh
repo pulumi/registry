dd03a5438a5bfde1eda61d6a547827e9738814c9
Christian Nunciato <c@nunciato.org>
2024-09-05 09:53:01 -0700

    Add a script to apply temporary, targeted API docs fixes (#5326)
    * Add a script to apply fixes
    
    * Add some comments

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,4 +4,7 @@ set -o errexit -o pipefail
 
 source ./scripts/common.sh
 
+# Apply fixes. These are things we do to patch known-malformed
+node scripts/apply-fixes.js
+
 hugo | grep -v -e 'WARN .* REF_NOT_FOUND'

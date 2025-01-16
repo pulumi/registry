699d23ff99f2a475b814163a17791f8518889ff5
Ian Wahbe <me@iwahbe.com>
2025-01-16 12:14:52 +0100

    Quote variables in `mktutorial.sh` (#6429)
    This is a cleanup pass on `mktutorial.sh`. It does not change intended functionality.

--- scripts/ci/mktutorial.sh
+++ scripts/ci/mktutorial.sh
@@ -2,27 +2,25 @@
 set -e
 TUTORIAL_OUT=$(mktemp -d)
 pushd ./tools/mktutorial
-go run *.go https://github.com/pulumi/examples $TUTORIAL_OUT
+go run ./*.go https://github.com/pulumi/examples "$TUTORIAL_OUT"
 popd
 for cloud in "aws-apigateway" "aws" "classic-azure" "azure" "gcp" "kubernetes" ; do
     ## Temporary hack to address mismatched package names in pulumi/examples
-    if [[ "$cloud" == "azure" ]]
-    then
+    if [[ "$cloud" == "azure" ]]; then
         dest="azure-native"
-    elif [[ "$cloud" == "classic-azure" ]]
-    then
+    elif [[ "$cloud" == "classic-azure" ]]; then
         dest="azure"
     else
-        dest=$cloud
+        dest="$cloud"
     fi
 
-    mkdir -p ./themes/default/content/registry/packages/$dest
-    mkdir -p ./themes/default/content/registry/packages/$dest/how-to-guides
+    mkdir -p "./themes/default/content/registry/packages/$dest"
+    mkdir -p "./themes/default/content/registry/packages/$dest/how-to-guides"
 
     # Check if the directory exists before copying.
     # (meaning there are tutorials for that cloud provider, if not the dir would not get created)
     if [ -d "$TUTORIAL_OUT/tutorials/$cloud" ]; then
-        cp $TUTORIAL_OUT/tutorials/$cloud/* ./themes/default/content/registry/packages/$dest/how-to-guides/
+        cp "$TUTORIAL_OUT/tutorials/$cloud/"* "./themes/default/content/registry/packages/$dest/how-to-guides/"
     fi
 done
-rm -rf $TUTORIAL_OUT
+rm -rf "$TUTORIAL_OUT"

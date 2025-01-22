99ccb625b8a6e9b49804862e080792b04d7ad9d6
Ian Wahbe <me@iwahbe.com>
2025-01-22 18:59:38 +0100

    Limit sets to valid values

--- tools/resourcedocsgen/pkg/util/language/set.go
+++ tools/resourcedocsgen/pkg/util/language/set.go
@@ -14,54 +14,86 @@
 
 package language
 
-import (
-	"iter"
-	"sort"
-)
+import "iter"
 
-type Set struct{ m map[Language]struct{} }
+// A set of [Language]s.
+//
+// Set is fully by-value, so it copies on move.
+type Set struct{ m uint16 }
 
-func (s Set) Has(l Language) bool { _, ok := s.m[l]; return ok }
+// Has returns if l is in s.
+func (s Set) Has(l Language) bool { return (s.mask(l) & s.m) != 0 }
 
-func (s *Set) Add(l Language) { s.m[l] = struct{}{} }
+// Add modifies s to contain l.
+func (s *Set) Add(l Language) { s.m = s.m | s.mask(l) }
 
-func (s Set) Remove(l Language) { delete(s.m, l) }
+// Remove modifies s to no longer contain l.
+func (s *Set) Remove(l Language) { s.m = s.m & ^s.mask(l) }
 
-func FullSet() Set { return NewSet(ListAll()...) }
+var fullSet = NewSet(ListAll()...)
 
+// FullSet returns a [Set] that contains all valid languages.
+func FullSet() Set { return fullSet }
+
+// NewSet creates a new [Set] that contains the languages in elems.
 func NewSet(elems ...Language) Set {
-	s := Set{m: map[Language]struct{}{}}
+	s := Set{}
 	for _, l := range elems {
 		s.Add(l)
 	}
 	return s
 }
 
+// langOrder describes the order that Set.Iter will return languages in.
+var langOrder = [...]Language{
+	CSharp,
+	Go,
+	Java,
+	Python,
+	Typescript,
+	YAML,
+}
+
+// Iter providers a deterministic iteration of the languages within s.
 func (s Set) Iter() iter.Seq[Language] {
-	items := make([]Language, 0, len(s.m))
-	for v := range s.m {
-		items = append(items, v)
-	}
-	sort.Slice(items, func(i, j int) bool {
-		return items[i].tag < items[j].tag
-	})
 	return func(yield func(Language) bool) {
-		for _, v := range items {
-			if !yield(v) {
-				return
+		for _, v := range langOrder {
+			if s.Has(v) {
+				if !yield(v) {
+					return
+				}
 			}
 		}
 	}
 }
 
-func (s Set) Copy() Set {
-	cp := Set{make(map[Language]struct{}, len(s.m))}
-	for l := range s.m {
-		cp.Add(l)
+func (s Set) IsEmpty() bool { return s.m == 0 }
+
+func (s Set) Len() int {
+	count := uint16(0)
+	for s.m > 0 {
+		count += s.m & 1
+		s.m = s.m >> 1
 	}
-	return cp
+	return int(count)
 }
 
-func (s Set) IsEmpty() bool { return s.Len() == 0 }
-
-func (s Set) Len() int { return len(s.m) }
+func (Set) mask(l Language) uint16 {
+	switch l {
+	case CSharp:
+		return 1
+	case Go:
+		return 1 << 1
+	case Java:
+		return 1 << 2
+	case Python:
+		return 1 << 3
+	case Typescript:
+		return 1 << 4
+	case YAML:
+		return 1 << 5
+	default:
+		// 0 will never add to a set, and will never show as present.
+		return 0
+	}
+}

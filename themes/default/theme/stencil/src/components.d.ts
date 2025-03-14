53eb0beb7780df5604fff88cf2bc15d36bf882d2
Simon Howe <footless@gmail.com>
2025-03-14 15:05:14 +0100

    Adds atlas (new-copilot) to Registry
    - As we recently added to the docs too

--- themes/default/theme/stencil/src/components.d.ts
+++ themes/default/theme/stencil/src/components.d.ts
@@ -14,6 +14,7 @@ import { Filter } from "./components/filter-select/filter-select-option";
 import { MultiSelectFormItem } from "./components/pulumi-multi-select-form/pulumi-multi-select-form";
 export namespace Components {
     interface CopilotSidebar {
+        "atlasUrl": string;
         "copilotSrc": string;
     }
     interface EventSessionRegistrationModal {
@@ -486,6 +487,7 @@ declare global {
 }
 declare namespace LocalJSX {
     interface CopilotSidebar {
+        "atlasUrl"?: string;
         "copilotSrc"?: string;
     }
     interface EventSessionRegistrationModal {

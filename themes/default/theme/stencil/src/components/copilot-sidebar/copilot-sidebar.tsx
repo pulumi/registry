5abe6316283d12c45311738271aca9fa55bda78a
Simon Howe <footless@gmail.com>
2025-04-10 11:35:11 +0200

    Copilot V2 as the default, rm Copilot V1

--- themes/default/theme/stencil/src/components/copilot-sidebar/copilot-sidebar.tsx
+++ themes/default/theme/stencil/src/components/copilot-sidebar/copilot-sidebar.tsx
@@ -1,7 +1,4 @@
 import { Component, h, Host, Prop, State } from "@stencil/core";
-import { gb } from "../../util/util";
-
-const ATLAS_LOCAL_STORAGE_KEY = "pulumi-ai-use-atlas";
 
 @Component({
     tag: "copilot-sidebar",
@@ -13,10 +10,6 @@ export class CopilotSidebar {
     // Props
     //
 
-    // The copilotSrc is URL where the copilot sidebar is hosted
-    @Prop()
-    copilotSrc: string;
-
     // The atlasUrl is the URL where the Atlas web component is hosted
     @Prop()
     atlasUrl: string;
@@ -25,35 +18,18 @@ export class CopilotSidebar {
     // Internal state
     //
 
-    // Whether to show the sidebar (growthbook flag)
-    @State()
-    showSidebar = false;
-
-    // Whether to show the Atlas web component (local storage flag)
-    @State()
-    showAtlas = false;
-
     // Whether the Atlas scripts have been loaded
     @State()
     atlasScriptsLoaded = false;
 
     async componentWillLoad() {
-        this.showSidebar = gb.isOn("copilot-on-docs");
-        gb.setRenderer(() => (this.showSidebar = gb.isOn("copilot-on-docs")));
-
-        this.showAtlas =
-            this.atlasUrl &&
-            window.localStorage.getItem(ATLAS_LOCAL_STORAGE_KEY) !== "false";
-        if (this.showAtlas) {
-            await this.loadAtlasScripts();
-        }
+        await this.loadAtlasScripts();
     }
 
     private async loadAtlasScripts() {
         const atlasWebComponentPolyfillsPath = this.atlasUrl + "/polyfills.js";
         const atlasWebComponentPath = this.atlasUrl + "/main.js";
         try {
-            // Then load Atlas scripts serially
             await this.loadScriptOnce(atlasWebComponentPolyfillsPath);
             await this.loadScriptOnce(atlasWebComponentPath);
             this.atlasScriptsLoaded = true;
@@ -81,26 +57,13 @@ export class CopilotSidebar {
     }
 
     render() {
-        if (this.showAtlas) {
-            if (!this.atlasScriptsLoaded) {
-                return <Host></Host>;
-            }
-
-            return (
-                <Host>
-                    <pulumi-copilot hostApp="docs" apiUrl={this.atlasUrl} />
-                </Host>
-            );
-        }
-
-        if (!this.showSidebar) {
+        if (!this.atlasScriptsLoaded) {
             return <Host></Host>;
         }
 
         return (
             <Host>
-                <aside id="ai-sidebar-target"></aside>
-                <iframe id="ai-sidebar-host" src={this.copilotSrc}></iframe>
+                <pulumi-copilot hostApp="docs" apiUrl={this.atlasUrl} />
             </Host>
         );
     }

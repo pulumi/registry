07b5a5aacaee7da0ae9e69a3bc81e3e80a523a4b
Sean Holung <sean.holung@gmail.com>
2025-04-16 17:26:59 -0700

    add to workflow

---
+++ scripts/ci/run-provider-tests.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -o errexit -o pipefail
+
+# runs on script exit to stop hugo server (whether successful or not)
+trap 'pkill -f "make serve" || true' EXIT
+
+pushd tools/resourcedocsgen
+go build -o "${GOPATH}/bin/resourcedocsgen" .
+popd
+
+resourcedocsgen docs --schemaFile scripts/tests/schema.json \
+    --version 1.0.0 \
+    --docsOutDir themes/default/content/registry/packages/test-provider/api-docs \
+    --packageTreeJSONOutDir "themes/default/static/registry/packages/navs"
+
+make serve &
+
+sleep 10
+
+./scripts/run-browser-tests.sh "http://localhost:1313" "test-provider-api-docs.cy.js"

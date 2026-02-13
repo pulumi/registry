0362c2888437b12018cf3a20efa4830a3b015e18
Christian Nunciato <c@nunciato.org>
2026-02-13 15:27:12 -0800

    Update mktutorial to handle deletions from pulumi/examples (#9889)
    * Update mktutorial to handle deletions from pulumi/examples
    
    * Temporarily comment the auto-merge step
    
    * Add redirects for deleted examples

--- scripts/ci/mktutorial.sh
+++ scripts/ci/mktutorial.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e
+
 TUTORIAL_OUT=$(mktemp -d)
 make bin/mktutorial
-bin/mktutorial "$1" "$TUTORIAL_OUT"
+bin/mktutorial generate "$1" "$TUTORIAL_OUT"
+
 for cloud in "aws-apigateway" "aws" "classic-azure" "azure" "gcp" "kubernetes" ; do
     ## Temporary hack to address mismatched package names in pulumi/examples
     if [[ "$cloud" == "azure" ]]; then
@@ -13,13 +15,33 @@ for cloud in "aws-apigateway" "aws" "classic-azure" "azure" "gcp" "kubernetes" ;
         dest="$cloud"
     fi
 
-    mkdir -p "./themes/default/content/registry/packages/$dest"
-    mkdir -p "./themes/default/content/registry/packages/$dest/how-to-guides"
+    dest_dir="./themes/default/content/registry/packages/$dest/how-to-guides"
+    src_dir="$TUTORIAL_OUT/tutorials/$cloud"
 
-    # Check if the directory exists before copying.
-    # (meaning there are tutorials for that cloud provider, if not the dir would not get created)
-    if [ -d "$TUTORIAL_OUT/tutorials/$cloud" ]; then
-        cp "$TUTORIAL_OUT/tutorials/$cloud/"* "./themes/default/content/registry/packages/$dest/how-to-guides/"
+    mkdir -p "$dest_dir"
+
+    # Copy generated tutorials if any exist for this cloud.
+    if [ -d "$src_dir" ]; then
+        cp "$src_dir/"* "$dest_dir/"
     fi
+
+    # Clean up stale tutorials.
+    bin/mktutorial cleanup "$src_dir" "$dest_dir"
 done
+
+# Also clean up stale tutorials in versioned package directories.
+for pkg in "aws-v6" "azure-native-v2" ; do
+    dest_dir="./themes/default/content/registry/packages/$pkg/how-to-guides"
+    [ -d "$dest_dir" ] || continue
+
+    # Determine which cloud this versioned package corresponds to.
+    if [[ "$pkg" == "aws-v6" ]]; then
+        src_dir="$TUTORIAL_OUT/tutorials/aws"
+    elif [[ "$pkg" == "azure-native-v2" ]]; then
+        src_dir="$TUTORIAL_OUT/tutorials/azure"
+    fi
+
+    bin/mktutorial cleanup "$src_dir" "$dest_dir"
+done
+
 rm -rf "$TUTORIAL_OUT"

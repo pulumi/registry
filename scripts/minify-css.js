b9d166e0558ad5fdd7dd59841282c97584098dc9
Jeff Merrick <jmerrick@pulumi.com>
2026-05-21 12:27:54 -0500

    Remove purgeCSS, it breaks Tailwind v4

--- scripts/minify-css.js
+++ scripts/minify-css.js
@@ -1,7 +1,6 @@
 const fs = require("fs");
 const glob = require("glob");
 const postcss = require("postcss");
-const { purgeCSSPlugin: purgecss } = require("@fullhuman/postcss-purgecss");
 const cssnano = require("cssnano");
 
 const registryBundleSlug = "-registry";
@@ -43,21 +42,6 @@ const bundles = [
     },
 ];
 
-const sharedSafelist = [
-    /^hs-/,
-    /^highlight$/,
-    /^pagination$/,
-    /^code-/,
-    /^copy-/,
-    /^carousel/,
-    /^st-/,
-    /^icon-/,
-    /^package-details/,
-    /^resources-properties/,
-    /^tabular/,
-    /^pulumi-/,
-];
-
 function minifyCSS(config) {
     const bundlePath = glob.sync(config.input)[0];
 
@@ -69,21 +53,6 @@ function minifyCSS(config) {
     const outputPath = `public/registry/css/${config.name}.${cssBundleId}.css`;
 
     return postcss([
-        // PurgeCSS removes unused CSS by analyzing the files of the built website.
-        // https://purgecss.com/
-        purgecss({
-            content: config.content,
-            css: [bundlePath],
-            safelist: { deep: sharedSafelist },
-
-            // We need to extract the Tailwind screen size selectors (e.g. sm, md, lg)
-            // so that we do not strip them out. As long as a class name appears in the HTML
-            // in its entirety, PurgeCSS will not remove it.
-            // Ex. https://tailwindcss.com/docs/optimizing-for-production#writing-purgeable-html
-            defaultExtractor: (content) =>
-                content.match(/[\w-/.:]+(?<!:)/g) || [],
-        }),
-
         // CSSNano minifies our rendered CSS by removing whitespace, comments, etc.
         // https://cssnano.co/
         cssnano({
